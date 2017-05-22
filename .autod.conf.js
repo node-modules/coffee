d8993b1ab1897ea0bb0740de52ca7b0ec46751b0
popomore <sakura9515@gmail.com>
2017-05-22 13:19:49 +0800

    deps: upgrade dependencies

---
+++ .autod.conf.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  devprefix: '^',
+  exclude: [
+    'test/fixtures',
+  ],
+  devdep: [
+    'autod',
+    'egg-ci',
+    'eslint',
+    'eslint-config-egg',
+  ],
+  keep: [
+  ],
+  semver: [
+  ],
+};
