95889eab194f29aa6e6ceb3b7488c5b9ceb3ae41
fengmk2 <fengmk2@gmail.com>
2017-06-28 17:46:14 +0800

    f

--- test/fixtures/mock-os/cli.js
+++ test/fixtures/mock-os/cli.js
@@ -1,7 +1,3 @@
 #!/usr/bin/env node
 
-
-require('/Users/mk2/git/github.com/popomore/coffee/test/fixtures/mock-os/mock.js'); // !!!!!!!coffee hook script, don't change it!!!!!!!!
-
-
 console.log('homedir = %s', require('os').homedir());
