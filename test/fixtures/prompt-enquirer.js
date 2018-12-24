dc7bc2b7f65ad3f4ca3e5590c1cccebc495d881b
wanghx <whx89768@alibaba-inc.com>
2018-12-24 11:21:34 +0800

    feat: change tty

--- test/fixtures/prompt-enquirer.js
+++ test/fixtures/prompt-enquirer.js
@@ -3,6 +3,7 @@
 'use strict';
 
 const { prompt } = require('enquirer');
+const { ReadStream } = require('tty');
 
 prompt.on('prompt', prompt => {
   // console.log(prompt);
@@ -10,20 +11,26 @@ prompt.on('prompt', prompt => {
 });
 
 async function run() {
+  const stdin = new ReadStream(process.stdin.fd);
+  process.stdin.pipe(stdin);
   const answers = await prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is your name?',
+      stdin,
     },
     {
       type: 'select',
       name: 'color',
       message: 'Pick a flavor',
       choices: [ 'apple', 'grape', 'watermelon', 'cherry', 'orange' ],
+      stdin,
     },
   ]);
   console.log(answers);
+  stdin.end();
+  process.stdin.end();
 }
 
 run().catch(console.error);
